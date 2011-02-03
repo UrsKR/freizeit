649ab0df534f1485c8da3567d44246a269db3f8f
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-02-03 16:53:40 +0100

    Nullwerte werden wieder ausgegeben.

--- grails-app/controllers/freizeit/Urlaubsantrag.groovy
+++ grails-app/controllers/freizeit/Urlaubsantrag.groovy
@@ -19,18 +19,6 @@ class Urlaubsantrag {
     if (typ == 'Erholungsurlaub') {
       resturlaub -= numberOfDays
     }
-    jahresanspruch != 0 ? resturlaub : null
-  }
-
-  def getJahresanspruch() {
-    getNullOrValue jahresanspruch
-  }
-
-  def getVorjahresanspruch() {
-    getNullOrValue vorjahresanspruch
-  }
-
-  private def getNullOrValue(int value) {
-    value != 0 ? value : null
+    jahresanspruch.toInteger() != 0 ? resturlaub : null
   }
 }
