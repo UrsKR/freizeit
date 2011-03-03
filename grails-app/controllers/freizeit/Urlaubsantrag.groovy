873e1d5406716d1648032ef1a84b5f2744696d33
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-03-03 10:35:24 +0100

    Die Zahl der benötigten Tage wird aus einem Service ermittelt.

--- grails-app/controllers/freizeit/Urlaubsantrag.groovy
+++ grails-app/controllers/freizeit/Urlaubsantrag.groovy
@@ -2,23 +2,22 @@ package freizeit
 
 class Urlaubsantrag {
 
-  def mitarbeiter
-  int vorjahresanspruch
-  int jahresanspruch
-  def typ
-  Date firstDay
-  Date lastDay
+    def mitarbeiter
+    int vorjahresanspruch
+    int jahresanspruch
+    def typ
+    Date firstDay
+    Date lastDay
 
-  int getNumberOfDays() {
-    new FeiertagService().getWorkdays(firstDay..lastDay)
-  }
-
-  def getResturlaub() {
-    int available = vorjahresanspruch + jahresanspruch
-    int resturlaub = available;
-    if (typ == 'Erholungsurlaub') {
-      resturlaub -= numberOfDays
+    int getNumberOfDays() {
+        new FeiertagService().getWorkdays(firstDay..lastDay)
     }
-    jahresanspruch.toInteger() != 0 ? resturlaub : null
-  }
+
+    def getResturlaub() {
+        int available = vorjahresanspruch + jahresanspruch
+        int resturlaub = available;
+        int daysRequired = new UrlaubstageService().getRequiredUrlaubstage(typ, firstDay..lastDay)
+        resturlaub -= daysRequired
+        jahresanspruch.toInteger() != 0 ? resturlaub : null
+    }
 }
