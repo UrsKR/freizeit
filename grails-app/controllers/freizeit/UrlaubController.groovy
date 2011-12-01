0ce126268089b618dabee1c9f0edfbdcf4a3984a
Urs Reupke <ur@idos.de>
2011-12-01 15:36:03 +0100

    Cookies for the remaining days and the beginning of a new year

--- grails-app/controllers/freizeit/UrlaubController.groovy
+++ grails-app/controllers/freizeit/UrlaubController.groovy
@@ -1,6 +1,8 @@
 package freizeit
 
 import java.text.SimpleDateFormat
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 class UrlaubController {
 
@@ -8,18 +10,31 @@ class UrlaubController {
     public static final String Cookie_Name = "Name"
     public static final String Cookie_Vorjahresanspruch = "Vorjahresanspruch"
     public static final String Cookie_Jahresanspruch = "Jahresanspruch"
+    public static final String Cookie_Antragsjahr = "Antragsjahr"
     static defaultAction = 'antrag'
     def urlaubstageService
     def pdfRenderingService
 
     def antrag = { Urlaubsantrag antrag ->
-        antrag.mitarbeiter = request.getCookie(Cookie_Name) ?: ""
-        antrag.vorjahresanspruch = request.getCookie(Cookie_Vorjahresanspruch) as Float ?: 0
-        antrag.jahresanspruch = request.getCookie("Jahresanspruch") as Float ?: 0
+        fillAntragFromCookies(antrag, request)
         def days = antrag.numberOfDays
         String text = getTextForNumberOfDays(days)
         flash.message = text
         [antrag: antrag]
+    }
+
+    private def fillAntragFromCookies(Urlaubsantrag antrag, HttpServletRequest request) {
+        antrag.mitarbeiter = request.getCookie(Cookie_Name) ?: ""
+        def lastRequestsYear = request.getCookie(Cookie_Antragsjahr) as int;
+        if (lastRequestsYear == currentYear) {
+            antrag.vorjahresanspruch = request.getCookie(Cookie_Vorjahresanspruch) as Float ?: 0
+            antrag.jahresanspruch = request.getCookie("Jahresanspruch") as Float ?: 0
+        } else {
+            float vorvorjahresanspruch = request.getCookie(Cookie_Vorjahresanspruch) as Float ?: 0
+            float vorjahresanspruch = request.getCookie("Jahresanspruch") as Float ?: 0
+            antrag.vorjahresanspruch = vorvorjahresanspruch + vorjahresanspruch
+            antrag.jahresanspruch = 28
+        }
     }
 
     def requiredDays = {
@@ -33,12 +48,21 @@ class UrlaubController {
     }
 
     def pdf = { Urlaubsantrag antrag ->
+        storeResultInCookies(response, antrag)
+        pdfRenderingService.render([template: '/urlaub/pdf', model: [antrag: antrag]], response)
+    }
+
+    private def storeResultInCookies(HttpServletResponse response, Urlaubsantrag antrag) {
         response.setCookie(Cookie_Name, antrag.mitarbeiter, oneYearInSeconds)
         float restVorjahresanspruch = Math.max(antrag.vorjahresanspruch - antrag.numberOfDays, 0)
         response.setCookie(Cookie_Vorjahresanspruch, restVorjahresanspruch as String, oneYearInSeconds)
         float restJahresanspruch = restVorjahresanspruch > 0 ? antrag.jahresanspruch : antrag.jahresanspruch - (antrag.numberOfDays - antrag.vorjahresanspruch)
         response.setCookie(Cookie_Jahresanspruch, restJahresanspruch as String, oneYearInSeconds)
-        pdfRenderingService.render([template: '/urlaub/pdf', model: [antrag: antrag]], response)
+        response.setCookie(Cookie_Antragsjahr, currentYear as String, oneYearInSeconds)
+    }
+
+    private int getCurrentYear() {
+        return new Date().year
     }
 
     private String getTextForNumberOfDays(days) {
